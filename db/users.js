c6de40f2f4464bc009c0bdb376d70238b7b53428
Mohammed Abrar <mohammed.abrar95@gmail.com>
2017-04-24 15:09:06 +0530

    Update users.js

--- db/users.js
+++ db/users.js
@@ -1,7 +1,7 @@
 var mongo = require('mongodb');
 var monk = require('monk');
-//var db = monk('mongodb://mohammed_abrar95:BPEJTwZgEYgKwixL@myapplicationdatabase-shard-00-00-gmb8r.mongodb.net:27017,myapplicationdatabase-shard-00-01-gmb8r.mongodb.net:27017,myapplicationdatabase-shard-00-02-gmb8r.mongodb.net:27017/MyDatabase?ssl=true&replicaSet=MyApplicationDatabase-shard-0&authSource=admin');
-var db = monk('localhost:27017/MyApplicationDatabase');
+var db = monk('mongodb://mohammed_abrar95:BPEJTwZgEYgKwixL@myapplicationdatabase-shard-00-00-gmb8r.mongodb.net:27017,myapplicationdatabase-shard-00-01-gmb8r.mongodb.net:27017,myapplicationdatabase-shard-00-02-gmb8r.mongodb.net:27017/MyDatabase?ssl=true&replicaSet=MyApplicationDatabase-shard-0&authSource=admin');
+//var db = monk('localhost:27017/MyApplicationDatabase');
 var records = [];
 
 //db.collection('users').insert({ _id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] });	
