d1974588fe8c75deedf3b102417fe667fd3a5264
Mohammed Abrar <mohammed.abrar95@gmail.com>
2017-05-10 14:59:44 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var watson = require('watson-developer-cloud');
 var mongo = require('mongodb');
 var monk = require('monk');
 var http = require('http');
-var db = monk('localhost:27017/MyApplicationDatabase');
-//var db = monk('mongodb://mohammed_abrar95:BPEJTwZgEYgKwixL@myapplicationdatabase-shard-00-00-gmb8r.mongodb.net:27017,myapplicationdatabase-shard-00-01-gmb8r.mongodb.net:27017,myapplicationdatabase-shard-00-02-gmb8r.mongodb.net:27017/MyDatabase?ssl=true&replicaSet=MyApplicationDatabase-shard-0&authSource=admin');
+//var db = monk('localhost:27017/MyApplicationDatabase');
+var db = monk('mongodb://mohammed_abrar95:BPEJTwZgEYgKwixL@myapplicationdatabase-shard-00-00-gmb8r.mongodb.net:27017,myapplicationdatabase-shard-00-01-gmb8r.mongodb.net:27017,myapplicationdatabase-shard-00-02-gmb8r.mongodb.net:27017/MyDatabase?ssl=true&replicaSet=MyApplicationDatabase-shard-0&authSource=admin');
 router.get('/',
   function(req, res) {
     res.render('home');
